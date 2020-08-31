eb2c511cfeddf071f302f3201313b3facf190660
wow123456789 <68571872+wow123456789@users.noreply.github.com>
2020-08-31 11:30:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 function revealMessage(){document.getElementById("hiddenmessage").style.display='block';}
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+tabs.forEach(tab => {tab.addEventListener('click', () => {
+const target = document.querySelector(tab.dataset.tabTarget)
+TabContents.forEach(tabContent => {tabContent.classList.remove('active')})
+tabs.forEach(tab => {tab.classList.remove('active')})
+tab.classList.add('active')
+target.classList.add('active')
+function opentabs(my webpage,My Youtube channel,about) {
+var i, tabcontent, tablinks;
+tabcontent = document.getElementsByClassName("tabcontent");
+for (i = 0; i < tabcontent.length; i++) {tabcontent[i].style.display = "none";}
+tablinks = document.getElementsByClassName("tablinks");
+for (i = 0; i < tablinks.length; i++) {
+tablinks[i].className = tablinks[i].className.replace(" active", "");}
+document.getElementById(cityName).style.display = "block";
+evt.currentTarget.className += " active";}
